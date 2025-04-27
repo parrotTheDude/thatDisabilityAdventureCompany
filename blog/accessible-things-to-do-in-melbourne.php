3f1516e5ffff78a8661494c9d8afb516f0633943
Jacob <program.acquaint533@passinbox.com>
2025-04-28 08:18:09 +1000

    New blog and imgs

---
+++ blog/accessible-things-to-do-in-melbourne.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+      $page_title = 'Fun and Accessible Things to Do in Melbourne | TDAC Australia';
+      $page_decription = 'Discover the top 10 fun and accessible activities in Melbourne, perfect for individuals with mild to moderate disabilities. Explore Melbourne with ease and enjoyment!';
+      $page_name = '/blog/fun-things-to-do-melbourne';
+      include ('../inc/blogHead.php');
+    ?>
+  </head>
+
+  <body id="theTop">
+    <header>
+      <?php include ('../inc/header.php'); ?>
+    </header>
+
+    <main>
+      <?php include ('../inc/mobileNav.php'); ?>
+
+      <article>
+        <section class="articleTitleContainer">
+          <section class="textContainerSmaller">
+            <h1 class="articleTitle">Top 10 Fun and Accessible Things to Do in Melbourne</h1>
+            <h2>Inclusive Adventures for People with Mild to Moderate Disabilities</h2>
+            <span class="author">By Pari | 27th April 2025 | 15 min read</span>
+          </section>
+          <img src="img/blog/melbourne1.webp" class="articleImg" alt="Melbourne city skyline">
+        </section>
+
+        <div class="progressbarHead">
+          <div class="progress-container">
+            <div class="progress-bar" id="myBar"></div>
+          </div>
+        </div>
+
+        <section class="mainArticle">
+          <section class="textContainer">
+
+            <section class="paragraphSection">
+              <p>Melbourne is renowned for its vibrant culture, diverse attractions, and commitment to inclusivity. Whether you're seeking sensory-friendly environments, wheelchair-accessible venues, or simply a fun day out, Melbourne offers a wide range of activities that are both enjoyable and accessible.</p>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>1. ArtVo</h2>
+              <p>ArtVo turns the traditional art exhibition experience upside down with an immersive gallery that is loaded with optical illusions. At ArtVo you can put yourself inside the art, from oceans to outer space. Visitors are encouraged to touch and interact with the art, making it a unique and fun experience for all ages.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The 1400 square-metre space is wheelchair accessible</li>
+                <li>There are accessible toilets throughout the precinct, the closest to ArtVo is at the Star Circus</li>
+                <li>Free entry for carers with Companion/Carer Cards</li>
+                <li>The most convenient accessible parking is on the west side, off Pearl River Road</li>
+                <li>An accessible platform super stop is located right outside the shopping centre</li>
+                <li>The District’s customer service lounge offers complimentary wheelchair hire</li>
+              </ul>
+              <p><em>Tip: Tickets are valid for the day so you can go at your own pace and take a break if needed.</em></p>
+              <section class="imgContainerArticle">
+                <img src="img/blog/artvo.webp" alt="Interactive artwork at ArtVo">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>2. Melbourne Zoo</h2>
+              <p>Nestled in the heart of the city, Melbourne Zoo is home to over 320 animal species. It is one of four zoos that make up Zoos Victoria, which is a world-leading zoo-based conservation organisation, dedicated to fighting wildlife extinction.</p>
+              <p>Melbourne zoo offers animal talks and experiences all year round and is tailored for people of all ages and abilities.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The zoo boasts wheelchair-accessible pathways, parking, sensory-friendly maps, and quiet zones</li>
+                <li>Free entry for all carers who is a holder of a Child Disability Allowance Health Care Card, a Disability Pension Card or a Companion Card</li>
+                <li>Quiet zones: The zoo will typically be the quietest between 3:00pm-4:30pm on Monday to Friday.  During these times the zoo aims to minimise high sensory auditory experiences such as vehicle movement, PA announcements and maintenance works</li>
+                <li>The zoo offers an Access Key which has all the information you need to plan your visit written in a format that is accessible to all ages and abilities. You can download the PDF from their website at <a href="https://www.zoo.org.au/melbourne/accessibility/">https://www.zoo.org.au/melbourne/accessibility/</a></li>
+              </ul>
+              <p><em>Tip: To avoid crowds, try attending in Winter, when the zoo may be even quieter and more enjoyable with the potential for more intimate one-on-one experiences with the animals. </em></p>
+              <section class="imgContainerArticle">
+                <img src="img/blog/melbournezoo.webp" alt="Giraffe at Melbourne Zoo">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>3. ACMI</h2>
+              <p>From films to video games, ACMI (originally known as the Australian Centre for the Moving Image) celebrates screen culture past and present. Located at Federation Square in the heart of Melbourne's CBD, ACMI is easy to access and offers the opportunity to explore the world of movies through interactive exhibitions, film screenings, and special events.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>There are two entrances, but it's best to use the Flinders Street one</li>
+                <li>A series of lifts offer access to all levels of ACMI</li>
+                <li>Once inside, everything is accessible, including accessible toilets on two levels</li>
+                <li>Complimentary wheelchairs are available</li>
+                <li>Carers accompanying companion card holders get free entry to ACMI’s ticketed programs.</li>
+                <li>ACMI also has relaxed visits and audio described tours for people with sensory disabilities</li>
+              </ul>
+              <p><em>Tip: Once a month, ACMI has relaxed visits which involve all exhibitions in the museum being switched to ‘low sensory’ mode between 10 and 11.30am. This is suited for visitors with ASD, sensory sensitivities or anyone who would benefit from a quieter and less busy setting.</em></p>
+              <section class="imgContainerArticle">
+                <img src="img/blog/acmi.webp" alt="ACMI exhibition view">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>4. National Gallery of Victoria (NGV)</h2>
+              <p>NGV is Australia’s oldest and most visited art gallery which offers wheelchair access, sensory-friendly sessions, and Auslan-interpreted tours. Quiet viewing times are also scheduled, catering to visitors who prefer a more subdued environment.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The NGV is free to visit and Companion or Carer Card holders receive concession priced tickets to all exhibitions and programs, with a complimentary ticket for their companion</li>
+                <li>The gallery offers Exhibition Access Guides for each exhibition which are available to download from the relevant exhibition page at <a href="www.ngv.vic.gov.au">www.ngv.vic.gov.au</a></li>
+                <li>For major exhibitions, sensory maps are also available on the NGV website showing the locations of audio-visual content</li>
+                <li>Wheelchairs are available for free hire and wheelchair access is available in the auditorium, studios, education theatres and toilets, as well as throughout the gallery spaces</li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/ngv.webp" alt="National Gallery of Victoria entrance">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>5. Eureka Skydeck</h2>
+              <p>Experience panoramic views of Melbourne from the Eureka Skydeck, the Southern Hemisphere's highest observation deck at nearly 300 metres high.</p>
+              <p>Alongside the epic view, the Skydeck offers experiences including the VR Voyager Theatre with award-winning 6D chairs that take you through an immersive journey of Melbourne, and the Edge Experience which suspends a transparent glass cube over the edge of the skydeck for more daring visitors!</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The Skydeck is wheelchair accessible</li>
+                <li>The VR Voyager Theatre and the Edge Experience are also wheelchair accessible</li>
+                <li>Carers get free access</li>
+                <li>Accessible restrooms and parking are available on-site with discounted parking for Skydeck visitors located directly beneath the building at Wilson Parking Eureka Tower, 70 City Road.</li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/skydeck.webp" alt="View from Eureka Skydeck">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>6. All Things Equal Café</h2>
+              <p>Located in Melbourne's south-east, All Things Equal is a café that champions inclusivity by employing individuals with disabilities to proudly form their ‘all abilities crew’. The cafe champions an employment pathway program that is developed to act as a stepping stone towards mainstream employment, while boasting great food and top tier Melbourne coffee.</p>
+              <p>The venue is wheelchair accessible and offers a welcoming atmosphere, making it a great spot to enjoy a meal while supporting a meaningful cause.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The venue is wheelchair accessible</li>
+                <li>The cafe has a welcoming atmosphere for all</li>
+                <li>The menu is vegetarian and pescatarian</li>
+                <li>By visiting you’ll be enjoying a welcoming atmosphere for all abilities while supporting a great cause!</li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/allthingsequal.webp" alt="All Things Equal Cafe front view">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>7. SEA LIFE Melbourne Aquarium</h2>
+              <p>Discover the wonders of marine life at SEA LIFE Melbourne. See some of the world's most incredible animals, including adorable penguins and thousands of tropical fish. The aquarium offers unique experiences such as a private underwater dining experience and a once-in-a-lifetime penguin encounter tour.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The aquarium offers wheelchair access, sensory-friendly maps, and noise-cancelling headphones upon request</li>
+                <li>Companion Card holders receive complimentary entry, and assistance animals are welcome</li>
+                <li>Quiet periods are during the week (outside of school holidays) after 2pm</li>
+                <li>The aquarium offers a Night on the Reef experience which is accessible for all guests, and does not include smoke, haze or strobe lights.</li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/sealife.webp" alt="Penguins at SEA LIFE Melbourne">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>8. Dialogue in the Dark</h2>
+              <p>This immersive exhibition offers a unique experience by guiding visitors through environments in complete darkness, led by blind or low-vision guides. It's an enlightening activity that fosters empathy and understanding, all within a controlled and accessible setting.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>Wheelchair accessible with lift access</li>
+                <li>In partnership with organisations such as Guide Dogs Australia ensuring support for those with visual impairments</li>
+                <li>Alternative experiences - the venue can offer a light version of the dark experience for those who prefer not to be in total darkness</li>
+                <li>Reduced pricing for those with valid disability IDs and their companions</li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/dialogue.webp" alt="Dialogue in the Dark exhibition"></img>
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>9. Royal Botanical Gardens, Melbourne Gardens</h2>
+              <p>The Melbourne Gardens were founded in 1846 and extends across 38 hectares in the centre of the city on land that slopes to the river with trees, garden beds, lakes and lawns.</p>
+              <p>With free access attracting over 1,900,000 visitors annually, the Melbourne Gardens are a treasured part of the city.</p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>Travellers Aid Companion Service volunteers are available to support people with disability or those with additional mobility requirements travelling to Royal Botanic Gardens Melbourne, from Monday to Friday</li>
+                <li>Walkers, motorised and manual wheelchairs are welcome on all guided tours. Most areas of the Melbourne Gardens are accessible by walker and wheelchair</li>
+                <li>Accessible toilets and parking are available and can be found on the Melbourne Map on gardens’ website: <a href="http://rbg.vic.gov.au/">http://rbg.vic.gov.au/</a></li>
+              </ul>
+              <section class="imgContainerArticle">
+                <img src="img/blog/royalgardens.webp" alt="Royal Botanical Gardens Melbourne">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>10. Federation Square</h2>
+              <p>As Melbourne's cultural hub, Federation Square is home to various events, galleries, and eateries. The square buzzes with a daily program and is the cultural heart of the city. The program is ever-changing, so check out the ‘Fed Square’ website to see what’s on: <a href="https://fedsquare.com/">https://fedsquare.com/</a></p>
+              <h3>Accessibility and good-to-knows:</h3>
+              <ul>
+                <li>The Main Square is made up of sandstone cobblestones, with a marked access path</li>
+                <li>Swanston Street offers the best access to the Main Square, and lift access is available to and from The Atrium</li>
+                <li>The Big Screen is equipped with a hearing loop and there are accessible toilets located next to Time Out Café</li>
+                <li>Quiet spaces and quiet experiences (such as the silent cinema) are available for those seeking a break from the bustling environment</li>
+              </ul>
+              <p><em>Tip: Although the majority of the space is accessible to wheelchairs, some specific experiences may not have access, check the website for each experience to make sure before you visit</em></p>
+              <section class="imgContainerArticle">
+                <img src="img/blog/fedsquare.webp" alt="Federation Square Melbourne">
+              </section>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>General Tips for a Comfortable Experience</h2>
+              <h3>Companion Cards:</h3>
+              <p>Many venues in Melbourne offer free or discounted entry for Companion Card holders.</p>
+              <h3>Quiet Times:</h3>
+              <p>Some attractions provide designated quiet hours or sensory-friendly sessions. It's advisable to check their websites or contact them in advance.</p>
+              <h3>Public Transport:</h3>
+              <p>Melbourne's public transport system is progressively becoming more accessible, with features like low-floor trams and accessible train stations. The Navilens app aids vision-impaired commuters by providing real-time information.</p>
+              <h3>Travellers Aid:</h3>
+              <p>Located at key train stations, Travellers Aid offers support services, including mobility equipment hire and rest areas.</p>
+            </section>
+
+            <section class="paragraphSection">
+              <h2>Final Thoughts</h2>
+              <p>Melbourne's dedication to inclusivity ensures that everyone, regardless of ability, can enjoy the city's rich assortment of experiences. Whether you're a local or a visitor, these accessible attractions promise memorable moments filled with joy and discovery for all!</p>
+              <p>That Disability Adventure Company (TDAC) offers regular trips to Melbourne’s top destinations, led by a fun and friendly group of young support workers and individuals with disabilities. Experience the best of Melbourne while building a sense of community, inclusivity, and adventure — all with the support of the TDAC team.</p>
+              <p>If this sounds like something you’d love to be part of, get in touch today! Simply fill out the form below, and one of our friendly support workers will reach out to you.</p>
+              <section class="btnsContainer">
+                <div class="topBtns accentBlueBg indexVideo">
+                  <a href="https://thatdisabilityadventurecompany.com.au/contact" class="linkBtn" id="whiteTxt">Get in touch</a>
+                </div>
+              </section>
+            </section>
+          </section>
+        </section>
+      </article>
+    </main>
+
+    <?php include ('../inc/footer.php'); ?>
+
+    <script>
+      <?php include ('../inc/js/coreJs.php'); ?>
+    </script>
+  </body>
+</html>
